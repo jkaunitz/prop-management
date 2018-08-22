89e919b9f833f4e535360b81cb197e94d6817384
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-08-22 15:06:01 -0700

    tabnav grid

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -4,7 +4,14 @@ class TabNav extends Component {
     render() {
         return (
             <div className='tab-nav'>
-        
+                <div className='tab-name__tabs'>
+                {
+                    this.props.tabs.map((tab, index) => {
+                        return <a className='tab-nav_tabs'>{tab.title}</a>
+                    })
+                }
+                </div>
+                <div>requests or newsletters content goes here</div>
             </div>
         )
     }

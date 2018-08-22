3ea8cbb32d714c6ebb9a6ad5d551408c5086f704
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-08-21 18:02:00 -0700

    built signup form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+
 import SigninForm from './signinForm';
 
 class Signin extends Component {
     render() {
         return (
-            <div className='sign-in'>
-                
+            <div className='sign-in'>                
                 <SigninForm/>
             </div>
         )

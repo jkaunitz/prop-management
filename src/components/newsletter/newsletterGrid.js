99315a12ca6a78898fb01a67726b0579e0695d92
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-08-24 14:33:31 -0700

    built archive and archive item components and placed on grid

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
 import NewsletterBox from './newsletterBox';
+import NewsletterArchive from './newsletterArchive';
 
 class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
                 {/* add button */}
-                <NewsletterBox date={new DAte()}/>
-                {/* archive */}
+                <NewsletterBox date={new Date()}/>
+                <NewsletterArchive/>
                 {/* content */}
             </div>
         )

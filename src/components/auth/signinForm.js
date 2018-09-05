282df03cd5f7e0c78debb0d948cafabc79e838ac
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-09-04 17:05:05 -0700

    changed to quotes

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
-import { FormTitle } from '../formTitle';
-import { FormInput, FormButton } from '../formFields';
-import TextLink from '../textLink';
+import { FormTitle } from "../formTitle";
+import { FormInput, FormButton } from "../formFields";
+import TextLink from "../textLink";
 
 class SigninForm extends Component {
     render() {
@@ -47,7 +47,7 @@ class SigninForm extends Component {
 }
 
 SigninForm = reduxForm({
-    form: 'signin'
+    form: "signin"
 })(SigninForm);
 
 export default SigninForm;

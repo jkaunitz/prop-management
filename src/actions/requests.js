7d43590805bd0bd4f4f73f9b07cca7d93104ca22
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-09-06 13:40:45 -0700

    snatched jwt auth token from local storage and provided createnewrequest with userid as well

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -14,8 +14,10 @@ export function changeSelectedRequestType(boxType) {
     )
 };
 
-export function createNewRequest(newRequest, success) {
-    return function() {
+export function createNewRequest(userId, newRequest, success) {
+    console.log('token:',localStorage.getItem('token'))
+    console.log('userId:', userId);
+    /*return function() {
         axios.post(`${ROOT_URL}/requests/new`, newRequest)
             then(response => {
                 console.log(response.data);
@@ -24,5 +26,5 @@ export function createNewRequest(newRequest, success) {
             .catch(err => {
                 console.log(err);
             })
-    }
+    }*/
 }

3a3483cddee5694588b90cc5c31d5f7c70c8b334
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-09-06 13:02:38 -0700

    refactored newsletternewform to take in custom data

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -4,7 +4,6 @@ import NewNewsletterForm from './newsletterNewForm';
 
 class NewNewsletter extends Component {
     onSubmit = (fields) => {
-
         // if(button == 'submit') {
             // save new newsletter on the backend. perform a post request here.
         //    console.log('trying to submit to backend.');
@@ -23,6 +22,10 @@ class NewNewsletter extends Component {
                     onCancel={() => this.onCancel()} 
                     onSubmit={(event) => this.onSubmit(event)}
                     formTitle='New Newsletter'
+                    fieldOnePlaceholder='Newsletter Title'
+                    fieldOneTitle='Newsletter Title'
+                    fieldTwoPlaceholder='Body Here'
+                    fieldTwoTitle='Body'
                 />
             </div>
         );
